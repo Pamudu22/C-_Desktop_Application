0e2c605920abe8575ae717d3ade6872bce867fa8
Pamudu Wijayabandara <121758463+Pamudu22@users.noreply.github.com>
2024-08-02 10:05:18 +0530

    Add files via upload

---
+++ WebApplication1/WebApplication1/WebService1.asmx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebApplication1
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public decimal Addition(decimal a, decimal b) {
+            return a + b;
+        }
+
+        [WebMethod]
+        public decimal Subtract(decimal a, decimal b)
+        {
+            return a - b;
+        }
+
+        [WebMethod]
+        public decimal Multiplication(decimal a, decimal b)
+        {
+            return a * b;
+        }
+
+        [WebMethod]
+        public decimal Division(decimal a, decimal b)
+        {
+            return a / b;
+        }
+
+        [WebMethod]
+        public decimal Modules(decimal a, decimal b)
+        {
+            return a % b;
+        }
+    }
+}
